4a4daaf74a05b6fca774926592cf8040670bb23a
Christian Nagel <christian@christiannagel.com>
2016-06-23 11:40:34 +0200

    integrate ASP.NET MVC 4.6 with typescript

---
+++ integration/SampleWebApp/SampleWebApp/Source/hello.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="helloworld.ts" />
+
+$(function () {
+    $("#button1").click((ev : JQueryEventObject) => {
+        let name: string = $("#name").val();
+        let greeting: string = new helloworld().greet(name);
+        $("#div1").html(greeting);
+    });
+});
